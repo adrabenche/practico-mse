0f144903c00c354040d02b47a0ac54559718c63b
Alejandro Drabenche <alejandro.drabenche@header0.com>
2023-01-04 15:54:23 -0300

    Version funcional

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const app = express();
+
+MONGO_HOST='mongodb://' + process.env.MONGO_HOST + ':27017/todo_express'
+
+//mongodb
+mongoose.connect(MONGO_HOST,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+
+    })
+    .then(() => console.log('MongoDB Conectada'))
+    .catch(err => console.log(err)
+);
+
+
+
+//middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+//rutes
+app.use(require('./routes/index'));
+app.use(require('./routes/todo'));
+
+//server connection
+app.listen(3000, () => console.log("Server on port: 3000 | Mongo: " + process.env.MONGO_HOST));
